ea739c9cc2c70feb515480a60200effeb802b587
Kenan Erdogan <kenan.erdogan@gesis.org>
2017-11-24 08:47:59 +0100

    fix hub url

--- jhub_shibboleth_auth/shibboleth_auth.py
+++ jhub_shibboleth_auth/shibboleth_auth.py
@@ -29,7 +29,7 @@ class ShibbolethLoginHandler(RemoteUserLoginHandler):
             user_hash = md5(persistent_id.encode()).hexdigest()
             user = self.user_from_username(user_hash)
             self.set_login_cookie(user)
-            self.redirect(self.hub.hub_prefix)
+            self.redirect(self.hub.server.hub_prefix)
 
 
 class ShibbolethLogoutHandler(LogoutHandler):
